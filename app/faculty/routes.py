31e4215628fee6195cb525a9decc9e01c8cc5f37
pungpon <zemonhunter0@gmail.com>
2024-03-06 00:28:50 +0700

    test insert and query

--- app/faculty/routes.py
+++ app/faculty/routes.py
@@ -1,9 +1,28 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from app.faculty import faculty
+
+from app.database import Database
+
+db = Database().connect()
+mycursor = db.cursor()
 
 # หน้าหลัก หรือหน้าแรก path="/" (index) 
 @faculty.route('/')
 def index():
     # render index.html
-    return render_template("faculty/index.html")
+    mycursor.execute("""SELECT * FROM faculty""")
+    data = mycursor.fetchall()
+    return render_template("faculty/index.html", data=data)
+
+# test insert faculty
+@faculty.route('/test/<string:name>')
+def test_insert(name):
+    # render index.html
+    mycursor.execute("""
+                        INSERT INTO `faculty`(`id`, `name`, `stats`) 
+                        VALUES 
+                        (NULL,'{}',NULL)
+                        """.format(name))
+    db.commit()
+    return redirect(url_for('faculty.index'))
